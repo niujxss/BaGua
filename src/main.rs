cfdf611b0f13ef475d48d2acb8d32451ce01cc5b
pc <pc@honey.com>
2025-03-04 22:02:25 +0800

    增加卦象猜卦名的功能

--- src/main.rs
+++ src/main.rs
@@ -41,10 +41,15 @@ impl BaGua{
     }
 }
 
+fn menu(){
+    println!("================= 输入序号开始游戏 ======================");
+    println!("================= 1.根据编号说名字 ======================");
+    println!("================= 2.根据卦象说名字 ======================");
+    println!("======================================================");
+}
 
 fn main() {
-    println!("Hello, 欢迎学习先天八卦! 给出编号猜名字！！");
-    println!("例如： 编号1   乾卦");
+
 
     let bg_array:[BaGua;8] = [
         BaGua::new(1,"乾卦","乾三连",[1,1,1]),
@@ -56,9 +61,39 @@ fn main() {
         BaGua::new(7,"艮卦","艮覆碗",[1,0,0]),
         BaGua::new(8,"坤卦","坤六断",[0,0,0]),
         ];
+
+    menu();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let input:u8 = input.trim().parse().expect("Please type a number!");
+
+    loop {
+        match input {
+            1 => {
+                play_a(&bg_array);
+            },
+            2 => {
+                play_b(&bg_array);
+            },
+            _ => {
+                println!("请输入正确的编号");
+            },
+        }
+    }
+
+    // for item in bg_array.iter(){
+    //     item.show();
+    // }
+
+}
+
+fn play_a(bg_array:&[BaGua;8]) {
+    println!("Hello, 欢迎学习先天八卦! 给出编号答名字！！");
+    println!("例如： 编号1   乾卦");
     loop {
         let number = rand::thread_rng().gen_range(1..9);
-        println!("请猜编号是 {} 的卦象是什么？", number);
+        println!("请猜编号是 {} 的卦名是什么？", number);
 
         let item = bg_array.iter().find(|i| i.id == number).unwrap();
 
@@ -71,21 +106,44 @@ fn main() {
 
             match item.name.cmp(&guess) {
                 Ordering::Equal => {
-                    println!("猜对了");
+                    println!("答对了");
                     item.show();
                     break;
                 },
                 _ => {
-                    println!("猜错了；继续猜");
+                    println!("答错了；继续答");
                     continue;
                 },
             }
         }
     }
+}
 
+fn play_b(bg_array:&[BaGua;8]) {
+    println!("Hello, 欢迎学习先天八卦! 给出卦象答名字！！");
+    loop {
+        let number = rand::thread_rng().gen_range(1..9);
+        let item = bg_array.iter().find(|i| i.id == number).unwrap();
+        item.draw();
+        println!("请问这是什么卦？");
 
-    // for item in bg_array.iter(){
-    //     item.show();
-    // }
+        loop {
+            let mut guess = String::new();
+            io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+            let guess = guess.trim().to_string();
+
+            match item.name.cmp(&guess) {
+                Ordering::Equal => {
+                    println!("答对了");
+                    //item.show();
+                    break;
+                },
+                _ => {
+                    println!("答错了；继续答");
+                    continue;
+                },
+            }
+        }
+    }
 }
